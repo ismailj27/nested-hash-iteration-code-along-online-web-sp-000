3148e2cfa6fc43d4e59615cdf1ac33b62438957f
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-31 19:40:59 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   flavors_Array = contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
   flavors_Array.each do |flavor|
     if flavor == "strawberry"
-      flavors.delete("strawberry")
+      flavor.delete("strawberry")
     end
   end
 end
